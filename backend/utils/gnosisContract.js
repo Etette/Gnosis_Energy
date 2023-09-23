cb2af8d94eb870debbcfd246bd63d56722356767
Etette Etok <etettetok5@gmail.com>
2023-09-23 17:53:06 +0100

    updated util functions

--- backend/utils/gnosisContract.js
+++ backend/utils/gnosisContract.js
@@ -1,13 +1,28 @@
 const {ethers, Wallet} = require('ethers');
 const contract_abi = require('../GnosisEnergy.json');
+const {account} = require('../routes/routes');
 const XDai = require('../utils/utils');
 const dotenv = require('dotenv');
 dotenv.config({path: '../.env'});
 
 
+// const initializeProvider = async() => {
+//     // Check if MetaMask is installed and available
+//     if (typeof window.ethereum !== 'undefined') {
+//       // Use MetaMask as a provider
+//       const provider = new ethers.providers.Web3Provider(window.ethereum);
+//       return provider;
+//     } else {
+//       throw new Error('MetaMask is not installed or not available.');
+//     }
+// }
+
 // Connect to an Ethereum node
-const _provider = process.env.PROVIDER
+const _provider = process.env.PROVIDER;
+console.log(_provider);
+
 const provider = new ethers.JsonRpcProvider(_provider);
+console.log(provider);
 
 // The address of smart contract
 const contractAddress = process.env.CONTRACT_ADDRESS;
@@ -16,26 +31,45 @@ const contractAddress = process.env.CONTRACT_ADDRESS;
 abi = contract_abi.abi;
 const contract = new ethers.Contract(contractAddress, abi, provider);
 
-const mnemonic = process.env.MNEMONIC;
-const mnemonicWallet = Wallet.fromPhrase(mnemonic);
+// const mnemonic = process.env.MNEMONIC;
+// const mnemonicWallet = Wallet.fromPhrase(mnemonic);
+// console.log('wallet address', mnemonicWallet.address);
 
-const signer = new ethers.Wallet(mnemonicWallet.privateKey, provider);
-const signedContract = new ethers.Contract(contractAddress, abi, signer);
+const Fixtures = async () => {
+    try {
+        const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+        const _signer = accounts[0];
+        console.log('signer',_signer);
+        return _signer;
+    } catch (e) {
+        return {error: e.message};
+    }
+}
 
+const _signer = account;
+//const signedContract = new ethers.Contract(contractAddress, abi, signer);
+
+// const _Signer = null;
+// const _signer = new ethers.Wallet(_Signer.privateKey, provider);
+const signedContract = new ethers.Contract(contractAddress, abi, _signer);
 
 
 exports.getUserTransactions = async (address) => {
   try {
     const result = await contract.getUserTotalPayment(address);
     if (result > 0){
+        //console.log(XDai.formatXDai(result.toString()));
         return XDai.formatXDai(result.toString());
     }
+    //console.log(result);
     return result.toString();
   } catch (error) {
     console.error("Error interacting with contract:", error.message);
     return {success: false, Err: error};
   }
 }
+
+// console.log(getUserTransactions(mnemonicWallet));
 
 exports.getPrice = async () => {
     try{
--- backend/utils/utils.js
+++ backend/utils/utils.js
@@ -15,3 +15,4 @@ exports.generateRandom16DigitNumber = () => {
     const max = 9e15;
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
